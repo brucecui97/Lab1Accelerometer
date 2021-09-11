1d7799ff293cc29e19e0cc63b8746bfff04c753b
bruce <brucec@hotmail.com>
2021-09-11 16:45:18 -0700

    update timer interval

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -134,7 +134,7 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 500;
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // debugTxtBox
