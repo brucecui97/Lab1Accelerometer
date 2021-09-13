5e51f5d9f6b837fa54d36acdde8ef0c3b378833a
bruce <brucec@hotmail.com>
2021-09-12 18:58:43 -0700

    add code to record accelermoteter data

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -5,10 +5,18 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+    enum AccelerationAxis
+    {
+        Ax,
+        Ay,
+        Az,
+        Unknown,
+    }
     public partial class Form1 : Form
     {
         SerialPort serialPort1 = new SerialPort("portNameNotSet", 9600, Parity.None, 8, StopBits.One);
         ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
+        AccelerationAxis nextAccelerationAxis = AccelerationAxis.Unknown;
 
         public Form1()
         {
@@ -43,8 +51,34 @@ namespace WindowsFormsApp1
             while (bytesToRead != 0)
             {
                 serialBytesToReadTxtBox.Text = serialPort1.BytesToRead.ToString();
+
                 int newByte = serialPort1.ReadByte();
+                if (nextAccelerationAxis == AccelerationAxis.Unknown)
+                {
+                    if (newByte == 255)
+                    {
+                        nextAccelerationAxis = AccelerationAxis.Ax;
+                    }
+                }
+                else if (nextAccelerationAxis == AccelerationAxis.Ax)
+                {
+                    AxTxtBox.Text = newByte.ToString();
+                    nextAccelerationAxis = AccelerationAxis.Ay;
+                }
+
+                else if (nextAccelerationAxis == AccelerationAxis.Ay)
+                {
+                    AyTxtBox.Text = newByte.ToString();
+                    nextAccelerationAxis = AccelerationAxis.Az;
+                }
+
+                else if (nextAccelerationAxis == AccelerationAxis.Az) {
+                    AzTxtBox.Text = newByte.ToString();
+                    nextAccelerationAxis = AccelerationAxis.Unknown;
+                }
+
                 dataQueue.Enqueue(newByte);
+
                 bytesToRead = serialPort1.BytesToRead;
             }
 
