a23521085d8109ccf0cb2a6f246fdbd8d4ef51db
bruce <brucec@hotmail.com>
2021-09-14 19:13:11 -0700

    update UI for state machine testing program

--- WindowsFormsApp1/WindowsFormsApp1/StateMachineTester.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/StateMachineTester.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Form2
+    partial class StateMachineTester
     {
         /// <summary>
         /// Required designer variable.
--- WindowsFormsApp1/WindowsFormsApp1/StateMachineTester.cs
+++ WindowsFormsApp1/WindowsFormsApp1/StateMachineTester.cs
@@ -8,9 +8,9 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form2 : Form
+    public partial class StateMachineTester : Form
     {
-        public Form2()
+        public StateMachineTester()
         {
             InitializeComponent();
         }
