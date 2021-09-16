8d1f350ad3f840a87bcb90cef3af7d1e50c04294
bruce <brucec@hotmail.com>
2021-09-16 15:39:56 -0700

    add stateMachineTesterHistory

---
+++ WindowsFormsApp1/WindowsFormsApp1/StateMachineTesterHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    class StateMachineTesterHistory
+    {
+        public Acceleration acceleration;
+        public int previousState;
+
+        public override string ToString()
+        {
+            return "(" + acceleration.ToString() + "," + previousState.ToString() + ")";
+        }
+    }
+
+ 
+}
