ac5ab2a6893525131eb37398f5d10271f7f34f17
bruce <brucec@hotmail.com>
2021-11-04 17:43:24 -0700

    add calculate rotationalspeed hz and rpm

--- WindowsFormsApp1/WindowsFormsApp1/EncoderDataHandler.cs
+++ WindowsFormsApp1/WindowsFormsApp1/EncoderDataHandler.cs
@@ -8,14 +8,27 @@ namespace WindowsFormsApp1
 {
     public class EncoderDataHandler
     {
+        private static readonly Double COUNT_PER_REVOLUTION = 100;
+        private static readonly Double SECONDS_BETWEEN_UART = 0.04;
+        private static readonly Double SIXTY_SECONDS_PER_MINUTE = 60;
 
         public Double calculateRotationalSpeedHz(EncoderData encoderData) {
-            return 0;
+
+            byte[] channelAByteArray =
+            { (byte) encoderData.channelADiffMSB, (byte) encoderData.channelADiffLSB};
+            double channelADiffCount = BitConverter.ToInt16(channelAByteArray, 0);
+
+            byte[] channelBByteArray =
+                { (byte) encoderData.channelBDiffMSB, (byte) encoderData.channelBDiffLSB};
+            double channelBDiffCount = BitConverter.ToInt16(channelBByteArray, 0);
+
+            return (channelADiffCount - channelBDiffCount) * (1 / COUNT_PER_REVOLUTION) / SECONDS_BETWEEN_UART;
+
         }
 
         public Double calculateRotationalSpeedRPM(EncoderData encoderData)
         {
-            return 0;
+            return calculateRotationalSpeedHz(encoderData)* SIXTY_SECONDS_PER_MINUTE;
         }
     }
 }
